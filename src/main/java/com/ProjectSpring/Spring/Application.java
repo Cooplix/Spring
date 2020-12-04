ac58cdcd851351f045d3e80866241a50342a67cb
Vadym <vadikdeputat@gmail.com>
2020-12-04 20:47:20 +0100

    add GreetingController.java add index.html add greeting.html

---
+++ src/main/java/com/ProjectSpring/Spring/Application.java
@@ -0,0 +1,13 @@
+package com.ProjectSpring.Spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
