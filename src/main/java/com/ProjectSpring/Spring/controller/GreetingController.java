afda0955e2f661a0c743b412462ffeeed70deb28
Vadym <vadikdeputat@gmail.com>
2020-12-07 21:21:55 +0100

    now all message have author

--- src/main/java/com/ProjectSpring/Spring/controller/GreetingController.java
+++ src/main/java/com/ProjectSpring/Spring/controller/GreetingController.java
@@ -2,7 +2,9 @@ package com.ProjectSpring.Spring.controller;
 
 import com.ProjectSpring.Spring.Repository.MessageRepository;
 import com.ProjectSpring.Spring.domain.Message;
+import com.ProjectSpring.Spring.domain.User;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -29,8 +31,12 @@ public class GreetingController {
     }
 
     @PostMapping("/main")
-    public String add(@RequestParam String text, @RequestParam String tag, Map<String, Object> model) {
-        Message message = new Message(text, tag);
+    public String add(
+            @AuthenticationPrincipal User user,
+            @RequestParam String text,
+            @RequestParam String tag,
+            Map<String, Object> model) {
+        Message message = new Message(text, tag, user);
 
         messageRepository.save(message);
 
--- src/main/java/com/ProjectSpring/Spring/domain/Message.java
+++ src/main/java/com/ProjectSpring/Spring/domain/Message.java
@@ -11,19 +11,24 @@ public class Message {
     private String text;
     private String tag;
 
+    @ManyToOne(fetch = FetchType.EAGER)
+    @JoinColumn(name = "user_id")
     private User author;
 
-    public Message(User author) {
-        this.author = author;
-    }
 
     public Message() {
     }
 
-    public Message(String text, String tag) {
+    public Message(String text, String tag, User user) {
         this.text = text;
         this.tag = tag;
+        this.author = user;
     }
+
+    public String getAuthorName() {
+        return author != null ? author.getUsername() : "<none>";
+    }
+
 
     public Integer getId() {
         return id;
@@ -47,5 +52,13 @@ public class Message {
 
     public void setTag(String tag) {
         this.tag = tag;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
     }
 }
